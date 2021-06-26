144e13098e345fa6105e49fef4e4a30f5c8a7edd
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-06-26 22:05:44 +0200

    FIX: trying to fix bug in build

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,8 @@
-const tailwindcss = require("tailwindcss");
-
 module.exports = {
     style: {
         postcss: {
             plugins: [
-                tailwindcss("./config/tailwind.config.js"),
+                require("tailwindcss"),
                 require("autoprefixer"),
             ],
         },
