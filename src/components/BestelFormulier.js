b1697226b5252f6c8980d393b1af2ce90c377280
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-06-27 19:19:12 +0200

    FEAT: added email sending on form submit with FormSubmit

--- src/components/BestelFormulier.js
+++ src/components/BestelFormulier.js
@@ -15,18 +15,41 @@ export default function BestelFormulier() {
         setState( prevState => ({ ...prevState, [fieldName]: evt.target.value }))
     };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log("Submitting", state);
-    }
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     console.log("Submitting", state);
+    // }
     
     const totalPrice = state.nrOfCrates*pricePerCrate;
+
+    const confirmationMessage=
+        (state.pickUpLocation === "TotalEnergies Winksele" 
+        ?
+        `Beste ${state.firstName} ${state.lastName}\n,
+Dank voor jouw bestelling.
+
+Zodra je de betaling hebt gedaan van € ${totalPrice} op rekeningnummer BE26 0637 0978 7229 van Deoudemarktisterugvanons met als mededeling "${state.firstName} ${state.lastName}: ${state.nrOfCrates} krat(ten)", krijg je van ons een bevestigingsmail van jouw betaling. Met die mail kan je jouw bier gaan halen bij TotalEnergies Winksele.
+
+Een dikke dank je wel.
+
+Bart, Johan, Kris, Piet en de kinderen van Ajpopoli.`
+        :
+        `Beste ${state.firstName} ${state.lastName},
+
+Dank voor jouw bestelling.
+
+Zodra je de betaling hebt gedaan van € ${totalPrice} op rekeningnummer BE26 0637 0978 7229 van Deoudemarktisterugvanons met als mededeling "${state.firstName} ${state.lastName}: ${state.nrOfCrates} krat(ten)", kan je jouw bier gaan ophalen bij ${state.pickUpLocation}.
+
+Een dikke dank je wel,
+
+Bart, Johan, Kris, Piet en de kinderen van Ajpopoli.`);
 
     return(
         <form 
             className="bg-white text-left mb-4 max-w-md mx-auto"
-            onSubmit={handleSubmit}
-            id="order-form"
+            // onSubmit={handleSubmit}
+            action="https://formsubmit.co/a818ae18f7f0497d0332703cbf54981f" 
+            method="POST"
         >
             <div className="m-4">
                 <label
@@ -38,9 +61,11 @@ export default function BestelFormulier() {
                 <input
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
                     type="text"
+                    name="Voornaam"
                     id="firstName"
                     onChange={setValue("firstName")}
                     value={state.firstName}
+                    required
                 />
             </div>
             <div className="m-4">
@@ -53,9 +78,11 @@ export default function BestelFormulier() {
                 <input
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
                     type="text"
+                    name="Achternaam"
                     id="lastName"
                     onChange={setValue("lastName")}
                     value={state.lastName}
+                    required
                 />
             </div>
             <div className="m-4">
@@ -68,9 +95,11 @@ export default function BestelFormulier() {
                 <input
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
                     type="email"
+                    name="email"
                     id="email"
                     onChange={setValue("email")}
                     value={state.email}
+                    required
                 />
             </div>
             <div className="m-4">
@@ -84,9 +113,11 @@ export default function BestelFormulier() {
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
                     type="number"
                     min={0}
+                    name="Aantal Bakken"
                     id="nrOfCrates"
                     onChange={setValue("nrOfCrates")}
                     value={state.nrOfCrates}
+                    required
                 />
             </div>
             <div className="m-4">
@@ -99,6 +130,7 @@ export default function BestelFormulier() {
                 <input
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
                     type="text"
+                    name="Totale prijs (incl. leeggoed)"
                     id="price"
                     value={`€ ${totalPrice || 0}` }
                     disabled
@@ -113,9 +145,11 @@ export default function BestelFormulier() {
                 </label>
                 <select
                     className=" bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 form-select block w-full mt-1 leading-normal"
+                    name="Afhaalkeuze"
                     id="pickUpLocation"
                     value={state.pickUpLocation}
                     onChange={setValue("pickUpLocation")}
+                    required
                 >
                     <option value="">-Maak uw keuze-</option>
                     <option value="Bart">via Bart</option>
@@ -134,19 +168,28 @@ export default function BestelFormulier() {
                 </label>
                 <textarea
                     className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                    name="Opmerkingen"
                     id="remarks"
                     onChange={setValue("remarks")}
                     value={state.remarks}
                     rows={4}
                 ></textarea>
             </div>
-            <div className="m-4 flex justify-center">
+            <div className="m-4 flex flex-col justify-center items-center">
                 <input
                     className=" bg-gold cursor-pointer text-white py-3 text-lg px-6 rounded-full transform hover:scale-110 filter hover:brightness-125 transition duration-300 ease-in-out"
                     type="submit"
                     value="Verzend bestelling"
                 />
+                <span>
+                    <p className="text-xs text-red">
+                    kijk uw gegevens goed na alvorens op verzenden te clicken
+                    </p>
+                </span>
             </div>
+            <input type="hidden" name="_subject" value="Nieuwe bestelling!"></input>
+            <input type="hidden" name="_autoresponse" value={confirmationMessage}></input>
+            <input type="hidden" name="_template" value="table"></input> {/*template for email sent to domitvo@outlook.com*/}
         </form>
     );
 };
